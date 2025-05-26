677fc14e388457d138ebb2e5589958a2244f53d4
Lucas Santos <lucassantos135b211@gmail.com>
2025-05-26 20:49:59 -0300

    atualização dos setters e getters

--- TP03LPR1.java
+++ TP03LPR1.java
@@ -1,7 +1,6 @@
 //Lucas da Silva Santos - CB3030598 - ADS371
 //Kaueh Farias Ferreira dos Santos - CB3031438 - ADS371
 
-
 import java.util.Scanner;
 
 class Hora{
@@ -12,132 +11,149 @@ class Hora{
 	private int min;
 	private int seg;
 	
-//Métodos Construtores
-
-public Hora(){
-	
 	Scanner scan = new Scanner(System.in);
 	
-	do{
-		
-		System.out.pritln("Digite o valor da hora: ");
-		public string hor = scan.nextInt();
-		
-		if(0 <= hor && hor >= 23) {
-			
-			this.hora = hor;
-			
-		} else{
-			
-			System.out.println("Digite um valor de hora valido!");
-			
-		}
-		
-	}while (hor < 0 || hor > 23);
-		
-	
-	do{
-		
-		System.out.pritln("Digite o valor da hora: ");
-		public string minu = scan.nextInt();
-		
-		if(0 <= minu && minu >= 59) {
-			
-			this.min = m;
-			
-		} else{
-			
-			System.out.println("Digite um valor de hora valido!");
-			
-		}
-		
-	}while (minu < 0 || minu > 59);
-	
-	do{
-		
-		System.out.pritln("Digite o valor da hora: ");
-		public string segu = scan.nextInt();
-		
-		if(0 <= segu && segu >= 59) {
-			
-			this.seg = segu;
-			
-		} else{
-			
-			System.out.println("Digite um valor de hora valido!");
-			
-		}
-		
-	}while (segu < 0 || segu > 59);
-	
-}
+//Métodos Construtores
 
-public Hora(int hora, int min, int seg){
-	
-	this.hora = hora;
-	this.min = min;
-	this.seg = seg;
-	
-}
-	
-	public void setHor(int hora){
+//vazio
+
+	public Hora(){
 		
-		this.hora = hora;
-	
-	}
-	
-	public int getHor(){
-	
-		return this.hora;
+		setHor();
+		setMin();
+		setSeg();
 		
 	}
-	
-	
-	public void setMin(int min){
-	
-		this.min = min; 
-	
-	}
-	
-	public int getMin(){
+
+//com parametros
+
+	public Hora(int hora, int min, int seg){
 		
-		return this.min;
+		setHor(hora);
+		setMin(min);
+		setSeg(seg);
 		
 	}
 	
-	
-	public void setSeg(int seg){
+//getters e setters com parametro
 		
-		this.seg = seg;
+		public void setHor(int hora){
+			
+			this.hora = hora;
 		
-	}
-	
-	public int getSeg(){
+		}
 		
-		return this.seg;
+		public void setMin(int min){
 		
-	}
-	
-	
-	public void setHor(){
-	
-	this.
-	
-	}
-	
-	public int gerHora1(){
+			this.min = min; 
 		
-		return this.Hor;
+		}
 		
-	}
+		public void setSeg(int seg){
+			
+			this.seg = seg;
+			
+		}
+		
+		public int getHor(){
+		
+			return this.hora;
+			
+		}
+		
+		public int getMin(){
+			
+			return this.min;
+			
+		}
+		
+		public int getSeg(){
+			
+			return this.seg;
+			
+		}
+		
+//setters sem parametro
+
+		public void setHor(){
+			
+			int hor;
+			
+			do{
+			
+			System.out.println("Digite o valor da hora: ");
+			hor = scan.nextInt();
+			
+			if(!scan.hasNextInt()){
+				
+				System.out.println("Entrada inválida! O valor deve ser um número");
+				
+			} else if(0 > hor || hor > 23) {
+				
+				System.out.println("Entrada inválida! O valor de hora deve estar entre 0 e 23");
+				
+			} else{
+				
+				this.hora = hor;
+			}
+			
+			}while (hor < 0 || hor > 23);
+			
+		}
+
+		public void setMin(){
+			
+			int minu;
+			
+			do{
+			
+			System.out.println("Digite o valor dos minutos: ");
+			minu = scan.nextInt();
+			
+			if(0 <= minu && minu <= 59) {
+				
+				this.min = minu;
+				
+			} else{
+				
+				System.out.println("Digite um valor de minutos valido!");
+				
+			}
+			
+			}while (minu < 0 || minu > 59);
+			
+		}
+
+		public void setSeg(){
+			
+			int segu;
+			
+			do{
+			
+			System.out.println("Digite o valor dos segundos: ");
+			segu = scan.nextInt();
+			
+			if(0 <= segu && segu <= 59) {
+				
+				this.seg = segu;
+				
+			} else{
+				
+				System.out.println("Digite um valor de segundos valido!");
+				
+			}
+			
+			}while (segu < 0 || segu > 59);
+			
+		}
+	
 }
 
 public class TP03LPR1{
 	
 	public static void main(String[] args){
 		
-		println("Digite o horário: ");
-		Scanner scan = new Scanner(System.in)
+	
 		
 		
 		
